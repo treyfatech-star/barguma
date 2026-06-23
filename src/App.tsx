7e7e7d922adc0bcd878319259d79a5b6e0e5e4ea
treyfatech-star <treyfatech@gmail.com>
2026-06-24 00:51:08 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "@/pages/Home";
 import About from "@/pages/About";
 import TrackRecord from "@/pages/TrackRecord";
